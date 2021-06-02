31feabeb488aabf11c29fb13f1bc4bc2e2d91fdd
Florian Donhauser <floriandonhauser@users.noreply.github.com>
2021-06-02 22:16:54 +0200

    Add create_policy function to create q_net and optimizer
    The create_policy function takes in an environment and creates a q_net
    and an Adam optimizer.
    The q_net is a simple example network with embedding, gru and output layer.
    The input is a single string and the output a vector with a value for each possible action.
    The main method performs a single call of the model for testing.

--- agent.py
+++ agent.py
@@ -1,53 +1,82 @@
-from typing import Mapping, Any, Optional
+from typing import Tuple
 import tensorflow as tf
+import tensorflow_hub as hub
+import tf_agents
+from tf_agents.networks import network
+from tf_agents.specs import array_spec
+from tf_agents.environments import random_py_environment, tf_py_environment
+import numpy as np
 
-class Agent(tf.keras.Model):
-    """
-    This class is an example agent used to play the game
-    """
-    def __init__(self, input_size: int):
-        super(Agent, self).__init__()
 
-    def call(self, inputs, **kwargs):
-        """
-        Method used in play to confirm to tf.keras.Model
+embedding = "https://tfhub.dev/google/nnlm-en-dim50/2"
+learning_rate = 1e-3
 
-        Parameters:
-        inputs: Input vector
 
+def create_policy(env) -> Tuple[tf_agents.networks.Network,
+                                tf_agents.typing.types.Optimizer]:
+    q_net = AgentNetwork(env.observation_spec(), env.action_spec())
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+    return q_net, optimizer
+
+
+class AgentNetwork(network.Network):
+    def __init__(self, input_tensor_spec, action_spec, name='ActorNetwork'):
+        super(AgentNetwork, self).__init__(
+            input_tensor_spec=input_tensor_spec, state_spec=(), name=name)
+
+        num_actions = action_spec.maximum - action_spec.minimum + 1
+        self.hub_layer = hub.KerasLayer(embedding, input_shape=[],
+                                        dtype=tf.string, trainable=True)
+        self.gru = tf.keras.layers.GRU(4, return_state=True)
+        self.q_value_layer = tf.keras.layers.Dense(
+            num_actions, activation=None)
+
+    def call(self, observation, network_state=None, training=False):
+        """A wrapper around `Network.call`.
+
+        Args:
+            inputs: The input to `self.call`, matching `self.input_tensor_spec`
+            network_state: A state to pass to the network used by the RNN layer
+            training: Optional argument to set to training mode
         Returns:
-        scores for the possible actions
+        A tuple `(outputs, new_network_state)`.
         """
-        scores = None
-        return scores
+        embedding = self.hub_layer(observation, training=training)
+        embedding = tf.expand_dims(embedding, axis=0)
+        gru_output, state = self.gru(embedding, initial_state=network_state)
+        q_value = self.q_value_layer(gru_output, training=training)
+        return q_value, state
 
-    def reset_hidden(self, batch_size: int):
-        """
-        This method is used to reset the hidden state of the model
-        """
-        pass
 
-    def act(self, state: Mapping[str, Any]) -> Optional[str]:
-        """
-        This method is used to find the action that should be performed in the current state
+def main():
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    for i in range(len(physical_devices)):
+        config = tf.config.experimental.set_memory_growth(
+            physical_devices[i], True)
+        print("Changed memory growth for", physical_devices[i])
 
-        Parameters:
-        state: Dictionary of strings describing the state
+    print("Running main in agent.py")
+    action_spec = array_spec.BoundedArraySpec(
+        (1,), np.int, minimum=0, maximum=10)
+    observation_spec = array_spec.ArraySpec((1,), np.str)
+    random_env = random_py_environment.RandomPyEnvironment(observation_spec=observation_spec,
+                                                           action_spec=action_spec)
+    # Convert the environment to a TFEnv to generate tensors.
+    tf_env = tf_py_environment.TFPyEnvironment(random_env)
+    print("Created environment")
+    # Create the policy as a q_net and an optimizer
+    q_net, optimizer = create_policy(tf_env)
+    print("Created q_net")
+    # Test the q_net with a test input
+    # TODO Maybe the random environment can create strings?
+    # Right now it fails if I simply calltf_env.reset()
+    observation = tf.constant(["This is a test string"])
+    q_value, state = q_net(observation)
+    print("the output")
+    print(q_value)
+    print("done")
 
-        Returns:
-        action that should be taken
-        """
-        action = None
-        return action
 
-    def save_network(self):
-        """
-        This method can be used to safe the model's weights on disk
-        """
-        pass
-
-    def load_network(self):
-        """
-        This method can be used to load the model's weights from disk
-        """
-        pass
+if __name__ == "__main__":
+    main()
