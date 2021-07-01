f99194dc1250024a8b0d78e280d73db1ec50d5e2
Max Lamparth <max.lamparth@tum.de>
2021-07-01 16:05:01 +0200

    feat: Added loop to game generation for creating standardized train/test set in future.

--- scripts/make_games.sh
+++ scripts/make_games.sh
@@ -2,7 +2,9 @@
 tw-make tw-simple --rewards dense --goal detailed --seed 2021 --output ../resources/game_simple_densedetailed.ulx
 tw-make tw-simple --rewards dense --goal brief --seed 2021 --output ../resources/game_simple_densebrief.ulx
 
-tw-make tw-coin_collector --level 3  --seed 2021 --output ../resources/game_coin_3.ulx
-tw-make tw-treasure_hunter  --level 3 --seed 2021 --output ../resources/game_treasure_3.ulx
-tw-make tw-coin_collector --level 5  --seed 2021 --output ../resources/game_coin_5.ulx
-tw-make tw-treasure_hunter  --level 5 --seed 2021 --output ../resources/game_treasure_5.ulx
+for i in {1..10}
+do
+   tw-make tw-treasure_hunter --level 1  --seed $i --output ../resources/gen_games/game_coin_lvl1_$i.ulx
+   tw-make tw-treasure_hunter --level 2  --seed $i --output ../resources/gen_games/game_coin_lvl2_$i.ulx
+   tw-make tw-treasure_hunter --level 3  --seed $i --output ../resources/gen_games/game_coin_lvl3_$i.ulx
+done
