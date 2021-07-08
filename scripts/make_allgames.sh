c2f09ef73d5ea5e09f479bc303664b5b680e4856
Max Lamparth <max.lamparth@tum.de>
2021-07-08 22:11:54 +0200

    feat: Added if statements to turn off game creations (ease of use)

--- scripts/make_allgames.sh
+++ scripts/make_allgames.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-for i in {1..1000}
-do
-   tw-make tw-treasure_hunter --level 1  --seed $i --output ../resources/train_games_lvl1/game_th_lvl1_$i.ulx
-   tw-make tw-treasure_hunter --level 2  --seed $i --output ../resources/train_games_lvl2/game_th_lvl2_$i.ulx
-   tw-make tw-treasure_hunter --level 3  --seed $i --output ../resources/train_games_lvl3/game_th_lvl3_$i.ulx
-   tw-make tw-treasure_hunter --level 4  --seed $i --output ../resources/train_games_lvl4/game_th_lvl4_$i.ulx
-done
+if True
+then
+  for i in {1..1000}
+  do
+     tw-make tw-treasure_hunter --level 1  --seed $i --output ../resources/train_games_lvl1/game_th_lvl1_$i.ulx
+     tw-make tw-treasure_hunter --level 2  --seed $i --output ../resources/train_games_lvl2/game_th_lvl2_$i.ulx
+     tw-make tw-treasure_hunter --level 3  --seed $i --output ../resources/train_games_lvl3/game_th_lvl3_$i.ulx
+     tw-make tw-treasure_hunter --level 4  --seed $i --output ../resources/train_games_lvl4/game_th_lvl4_$i.ulx
+  done
+fi
 
-for i in {1000..200}
-do
-   tw-make tw-treasure_hunter --level 1  --seed $i --output ../resources/test_games_lvl1/game_th_lvl1_$i.ulx
-   tw-make tw-treasure_hunter --level 2  --seed $i --output ../resources/test_games_lvl2/game_th_lvl2_$i.ulx
-   tw-make tw-treasure_hunter --level 3  --seed $i --output ../resources/test_games_lvl3/game_th_lvl3_$i.ulx
-   tw-make tw-treasure_hunter --level 4  --seed $i --output ../resources/test_games_lvl4/game_th_lvl4_$i.ulx
-done
+if True
+then
+  for i in {1000..1200}
+  do
+     tw-make tw-treasure_hunter --level 1  --seed $i --output ../resources/test_games_lvl1/game_th_lvl1_$i.ulx
+     tw-make tw-treasure_hunter --level 2  --seed $i --output ../resources/test_games_lvl2/game_th_lvl2_$i.ulx
+     tw-make tw-treasure_hunter --level 3  --seed $i --output ../resources/test_games_lvl3/game_th_lvl3_$i.ulx
+     tw-make tw-treasure_hunter --level 4  --seed $i --output ../resources/test_games_lvl4/game_th_lvl4_$i.ulx
+  done
+fi
