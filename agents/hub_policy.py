c26848a5c3abcb537a0195d25f0a8c88d99a4fc0
Max Lamparth <max.lamparth@tum.de>
2021-08-02 20:07:17 +0200

    feat: Updated simple FC agent to use batch norm and dropout.

--- agents/hub_policy.py
+++ agents/hub_policy.py
@@ -18,12 +18,19 @@ class HubPolicy(network.Network):
         self.num_obj = num_obj
 
         self.hub_layer = hub.KerasLayer(
-            embedding, input_shape=[], dtype=tf.string, trainable=False
+            embedding,
+            input_shape=[],
+            dtype=tf.string,
+            # embedding, input_shape = [], dtype = tf.string, trainable = False
         )
         # self.gru = tf.keras.layers.GRU(4, return_state=True)
 
         self.fc1 = tf.keras.layers.Dense(128, activation="relu")
         self.fc2 = tf.keras.layers.Dense(64, activation="relu")
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        self.do1 = tf.keras.layers.Dropout(0.1)
+        self.do2 = tf.keras.layers.Dropout(0.1)
 
         self.verb_layer = tf.keras.layers.Dense(num_verb, activation=None)
         self.obj_layer = tf.keras.layers.Dense(num_obj, activation=None)
@@ -44,18 +51,22 @@ class HubPolicy(network.Network):
             network_state = None
 
         flattened_observation = tf.reshape(observation, (-1))
-        # embedded_observations = self.hub_layer(flattened_observation, training=training)
-        embedded_observations = self.hub_layer(flattened_observation, training=False)
+        embedded_observations = self.hub_layer(flattened_observation, training=training)
+        # embedded_observations = self.hub_layer(flattened_observation, training=False)
 
         embedded_observations = tf.reshape(
             embedded_observations, (observation.shape[0], observation.shape[1], 128)
         )
 
-        out = self.fc1(embedded_observations, training=training)
+        out = self.bn1(embedded_observations, training=training)
+        out = self.fc1(out, training=training)
+        self.do1(out, training=training)
+        out = self.bn2(out, training=training)
         out = self.fc2(out, training=training)
-        gru_output = out
-        verb_q_value = self.verb_layer(gru_output, training=training)
-        obj_q_value = self.obj_layer(gru_output, training=training)
+        self.do2(out, training=training)
+
+        verb_q_value = self.verb_layer(out, training=training)
+        obj_q_value = self.obj_layer(out, training=training)
         q_value_multiplied = tf.matmul(verb_q_value, obj_q_value, transpose_a=True)
         q_values = tf.reshape(q_value_multiplied, (observation.shape[0], -1))
 
