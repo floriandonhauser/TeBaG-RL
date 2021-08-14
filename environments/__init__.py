19be1a25764bde4193a829ba327013c39b291802
Max Lamparth <max.lamparth@tum.de>
2021-08-14 12:16:24 +0200

    feat: Tidying up a few things before the deadline.

--- environments/__init__.py
+++ environments/__init__.py
@@ -1,2 +1,3 @@
 from environments.tf_game_env import TWGameEnv
 from environments.tf_create_environment import create_environments
+from environments.tf_vocab_collection_simple import run_auto_vocab
--- environments/tf_vocab_collection_simple.py
+++ environments/tf_vocab_collection_simple.py
@@ -4,10 +4,15 @@ import os
 from glob import glob
 from environments import create_environments
 
-if __name__ == "__main__":
+
+def run_auto_vocab():
+    """"""
 
     path = "../resources/train_games_lvl2/"
     gamefiles = glob(os.path.join(path, "*.ulx"))
-
     for game in gamefiles:
         create_environments(env_name=game, debug=True, expand_vocab=True, no_episodes=1)
+
+
+if __name__ == "__main__":
+    run_auto_vocab()
